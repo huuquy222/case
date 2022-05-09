aa6312c75af878fc1dee9d782b718b3a77864cba
huuquy222 <huuquynguyen274@gmail.com>
2022-05-09 13:24:39 +0700

    CS Ban Hang

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ class Product {
     }
 }
 let product = [
-    new Product("/icon1.jpg","Bút Kẽ Chân Mày","Sale 5%"),
-    new Product("/icon2.jpg","Kem Chống Nắng","Sale 5%"),
-    new Product("/icon3.jpg","Mặt Nạ Dưỡng Da","Sale 5%"),
-    new Product("/icon4.jpg","Phấn Má","Sale 5%"),
-    new Product("/icon5.jpg","Bộ Dụng Cụ Trang Điểm","Sale 5%"),
-    new Product("/icon6.jpg","Son Môi","2Sale 5%"),
-    new Product("/icon7.jpg","Kem Dưỡng Da","Sale 5%"),
-    new Product("/icon8.jpg","Bộ Dụng Cụ Kẹp Lông Mi","Sale 5%"),
-    new Product("/icon9.jpg","Son Nước","Sale 5%"),
-    new Product("/icon10.jpg","Bộ Dụng Cụ Trang Điểm Trên Má","Sale 5%"),
+    new Product("icon1.jpg","Bút Kẽ Chân Mày","Sale 5%"),
+    new Product("icon2.jpg","Kem Chống Nắng","Sale 5%"),
+    new Product("icon3.jpg","Mặt Nạ Dưỡng Da","Sale 5%"),
+    new Product("icon4.jpg","Phấn Má","Sale 5%"),
+    new Product("icon5.jpg","Bộ Dụng Cụ Trang Điểm","Sale 5%"),
+    new Product("icon6.jpg","Son Môi","2Sale 5%"),
+    new Product("icon7.jpg","Kem Dưỡng Da","Sale 5%"),
+    new Product("icon8.jpg","Bộ Dụng Cụ Kẹp Lông Mi","Sale 5%"),
+    new Product("icon9.jpg","Son Nước","Sale 5%"),
+    new Product("icon10.jpg","Bộ Dụng Cụ Trang Điểm Trên Má","Sale 5%"),
 ]
 function renderProduct(data){
     let html = data.map(function(products){
